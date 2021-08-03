6487054b40d31ba043f2dddbc5e196a435a91a5f
taekwon-dev <taekwon_dev@naver.com>
2021-08-03 10:29:13 +0900

    Recursion - fibonacci, factorials

---
+++ Algorithm/Algorithm Demo/src/com/ct/recursion/Factorials.java
@@ -0,0 +1,15 @@
+package com.ct.recursion;
+
+public class Factorials {
+    public static void main(String[] args) {
+        System.out.println("fact(5) = " + fact(5));
+    }
+
+    private static int fact(int n) {
+        if (n >= 1) {
+            return n * fact(n-1);
+        } else {
+            return 1;
+        }
+    }
+}
