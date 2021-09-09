70676b01b1639283181ac71de4f4739124275d27
taekwon-dev <taekwon_dev@naver.com>
2021-09-09 20:39:21 +0900

    2021/09/09

--- Algorithm/Algorithm Demo/src/com/ct/Main.java
+++ Algorithm/Algorithm Demo/src/com/ct/Main.java
@@ -4,5 +4,17 @@ public class Main {
     
     public static void main(String[] args) {
         // write your code here
+        int answer;
+        int n = 10;
+        answer = factorial(10);
+        System.out.println("answer = " + answer);
+
+    }
+    public static int factorial(int n) {
+        if (n==0) {
+            return 1;
+        } else {
+            return n*factorial(n-1);
+        }
     }
 }
