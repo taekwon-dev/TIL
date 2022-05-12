4d79bb96c17f64d618a573dc53e8eea90ea9d961
taekwon-dev <taekwon_dev@naver.com>
2022-05-13 00:47:10 +0900

    2022/05/12

---
+++ algorithm/src/com/til/algorithm/이코테/개미전사.java
@@ -0,0 +1,34 @@
+package com.til.algorithm.이코테;
+
+import java.util.Scanner;
+
+/**
+ * Created by Youn on 2022/05/12.
+ * Title : 개미 전사 (https://www.youtube.com/watch?v=5Lu34WIx2Us)
+ * Hint  :
+ */
+public class 개미전사 {
+
+    public static int[] dp = new int[100];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        dp[0] = arr[0];
+        dp[1] = Math.max(arr[0], arr[1]);
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + arr[i]);
+        }
+        System.out.println(dp[n - 1]);
+
+    }
+
+}
