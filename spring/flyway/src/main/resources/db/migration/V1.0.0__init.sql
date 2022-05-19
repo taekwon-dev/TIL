f40ee45c563d9dafbaf6e0abf2730caded6c788e
taekwon-dev <taekwon_dev@naver.com>
2022-05-19 15:17:23 +0900

    2022/05/19 V1.0.0__init.sql re-init

--- spring/flyway/src/main/resources/db/migration/V1.0.0__init.sql
+++ spring/flyway/src/main/resources/db/migration/V1.0.0__init.sql
@@ -5,21 +5,3 @@ CREATE TABLE user
     `password`  VARCHAR(60)    NOT NULL    COMMENT '유저 패스워드',
     PRIMARY KEY (id)
 );
-
-CREATE TABLE post
-(
-    `id`       BIGINT          NOT NULL    AUTO_INCREMENT COMMENT '포스트 ID',
-    `title`    VARCHAR(100)    NOT NULL    COMMENT '포스트 제목',
-    `content`  LONGTEXT        NOT NULL    COMMENT '포스트 내용',
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE post_likes
-(
-    `id`       BIGINT    NOT NULL    AUTO_INCREMENT COMMENT '포스트 좋아요 ID',
-    `user_id`  BIGINT    NULL        COMMENT '(좋아요 누른) 유저 ID',
-    `post_id`  BIGINT    NULL        COMMENT '(좋아요 대상) 포스트 ID',
-    PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE RESTRICT ON UPDATE RESTRICT,
-    FOREIGN KEY (post_id) REFERENCES post (id) ON DELETE RESTRICT ON UPDATE RESTRICT
-);
