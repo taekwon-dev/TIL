ce7558ef9916094707ebe280a8a184cbd7074ff5
taekwon-dev <taekwon_dev@naver.com>
2022-10-19 21:14:44 +0900

    2022/10/19

---
+++ algorithm/src/com/til/algorithm/baekjoon/shortest_path/BOJ_11265.java
@@ -0,0 +1,44 @@
+package com.til.algorithm.baekjoon.shortest_path;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_11265 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] dist = new int[n + 1][n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= n; j++) {
+                dist[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (dist[i][j] > dist[i][k] + dist[k][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+
+            if (dist[from][to] <= cost) {
+                System.out.println("Enjoy other party");
+            } else {
+                System.out.println("Stay here");
+            }
+        }
+    }
+}
--- algorithm/src/com/til/algorithm/baekjoon/shortest_path/BOJ_18352.java
+++ algorithm/src/com/til/algorithm/baekjoon/shortest_path/BOJ_18352.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-public class BOJ_1753 {
+public class BOJ_18352 {
     static class Node implements Comparable<Node> {
         int v;
         int cost;
@@ -20,50 +20,57 @@ public class BOJ_1753 {
 
         @Override
         public int compareTo(Node o) {
-            return this.cost - o.cost;
+            if (this.cost < o.cost) {
+                return -1;
+            }
+            return 1;
         }
     }
-    static int v, e, start;
-    static int[] dist;
+    static int n, m, k, x;
     static final int INF = (int) 1e9;
+    static int[] dist;
     static ArrayList<Node>[] adjList;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        v = Integer.parseInt(st.nextToken());
-        e = Integer.parseInt(st.nextToken());
-        start = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        x = Integer.parseInt(st.nextToken());
 
-        dist = new int[v + 1];
+        dist = new int[n + 1];
         Arrays.fill(dist, INF);
-        dist[start] = 0;
+        dist[x] = 0;
 
-        adjList = new ArrayList[v + 1];
-        for (int i = 1; i <= v; i++) {
+        adjList = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
             adjList[i] = new ArrayList<>();
         }
-        for (int i = 0; i < e; i++) {
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
-            adjList[from].add(new Node(to, cost));
-        }
-        dijkstra();
-        for (int i = 1; i < dist.length; i++) {
-            if (dist[i] == INF) {
-                System.out.println("INF");
-            } else {
-                System.out.println(dist[i]);
-            }
+            adjList[from].add(new Node(to, 1));
         }
 
+        dijkstra();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            if (dist[i] == k) {
+                sb.append(i + "\n");
+            }
+        }
+        if (sb.length() != 0) {
+            System.out.println(sb.toString());
+        } else {
+            System.out.println(-1);
+        }
     }
 
     private static void dijkstra() {
         PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.add(new Node(start, 0));
+        pq.add(new Node(x, 0));
         while (!pq.isEmpty()) {
             Node node = pq.poll();
 
@@ -80,4 +87,5 @@ public class BOJ_1753 {
             }
         }
     }
+
 }
