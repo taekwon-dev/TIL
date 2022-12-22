6e93fb509e697f1799df372b801df9ef501a2235
taekwon-dev <taekwon_dev@naver.com>
2022-12-22 17:31:29 +0900

    2022/12/22

---
+++ algorithm/src/com/til/algorithm/baekjoon/graph_traversal/BOJ_2468.java
@@ -0,0 +1,86 @@
+package com.til.algorithm.baekjoon.graph_traversal;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BOJ_2468 {
+
+    private static int n;
+    private static boolean[][] visited;
+    private static int[][] map;
+    private static int[] dx = {1, -1, 0, 0};
+    private static int[] dy = {0, 0, 1, -1};
+    private static int max = 1;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        map = new int[n][n];
+
+        StringTokenizer st = null;
+        int maxHeight = 1;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                int height = Integer.parseInt(st.nextToken());
+                map[i][j] = height;
+                maxHeight = Math.max(maxHeight, height);
+            }
+        }
+
+        for (int h = 1; h <= maxHeight; h++) {
+            flood(h);
+
+            int size = 0;
+            visited = new boolean[n][n];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (!visited[i][j] && map[i][j] != 0) {
+                        bfs(i, j);
+                        size++;
+                    }
+                }
+            }
+            max = Math.max(max, size);
+        }
+        System.out.println(max);
+    }
+
+    private static void bfs(int x, int y) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{x, y});
+        visited[x][y] = true;
+
+        while (!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            int currX = curr[0];
+            int currY = curr[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nx = currX + dx[i];
+                int ny = currY + dy[i];
+
+                if (nx >= 0 && ny >= 0 && nx < n && ny < n) {
+                    if (!visited[nx][ny] && map[nx][ny] != 0) {
+                        queue.add(new int[]{nx, ny});
+                        visited[nx][ny] = true;
+                    }
+                }
+            }
+        }
+    }
+
+    private static void flood(int height) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (map[i][j] <= height) {
+                    map[i][j] = 0;
+                }
+            }
+        }
+    }
+}
--- algorithm/src/com/til/algorithm/baekjoon/graph_traversal/BOJ_2583.java
+++ algorithm/src/com/til/algorithm/baekjoon/graph_traversal/BOJ_2583.java
@@ -5,15 +5,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class BOJ_2583 {
-    static int m, n, k;
-    static int[][] map;
-    static boolean[][] visited;
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
-    static int eachSize = 0;
+
+    private static int m;
+    private static int n;
+    private static int k;
+    private static boolean[][] visited;
+    private static int[][] map;
+    private static int[] dx = {1, -1, 0, 0};
+    private static int[] dy = {0, 0, 1, -1};
+    private static int size = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,8 +25,8 @@ public class BOJ_2583 {
         m = Integer.parseInt(st.nextToken());
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
-        map = new int[n + 1][m + 1];
-        visited = new boolean[n + 1][m + 1];
+        visited = new boolean[n][m];
+        map = new int[n][m];
 
         for (int i = 0; i < k; i++) {
             st = new StringTokenizer(br.readLine());
@@ -31,43 +35,41 @@ public class BOJ_2583 {
             int rx = Integer.parseInt(st.nextToken());
             int ry = Integer.parseInt(st.nextToken());
 
-            color(lx, ly, rx, ry);
-        }
-        ArrayList<Integer> result = new ArrayList<>();
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (!visited[i][j] && map[i][j] == 0) {
-                    dfs(i, j);
-                    result.add(eachSize);
-                    eachSize = 0;
+            for (int x = lx; x < rx; x++) {
+                for (int y = ly; y < ry; y++) {
+                    map[x][y] = 1;
                 }
             }
         }
-        Collections.sort(result);
-        System.out.println(result.size());
+
+        List<Integer> sizes = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!visited[i][j] && map[i][j] == 0) {
+                    size = 0;
+                    dfs(i, j);
+                    sizes.add(size);
+                }
+            }
+        }
+        System.out.println(sizes.size());
+        Collections.sort(sizes);
         StringBuilder sb = new StringBuilder();
-        for (int size : result) {
-            sb.append(size + " ");
+        for (int size : sizes) {
+            sb.append(size).append(" ");
         }
         System.out.println(sb.toString());
     }
 
-    private static void color(int lx, int ly, int rx, int ry) {
-        for (int i = lx + 1; i <= rx; i++) {
-            for (int j = ly + 1; j <= ry; j++) {
-                map[i][j] = 1;
-            }
-        }
-    }
-
     private static void dfs(int x, int y) {
         visited[x][y] = true;
-        eachSize++;
+        size++;
+
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
 
-            if (nx >= 1 && ny >= 1 && nx <= n && ny <= m) {
+            if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
                 if (!visited[nx][ny] && map[nx][ny] == 0) {
                     dfs(nx, ny);
                 }
