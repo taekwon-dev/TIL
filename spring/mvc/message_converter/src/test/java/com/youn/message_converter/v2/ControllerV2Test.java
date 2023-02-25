c63571aaad18a54399f8aa6e9bf94b908d077c21
taekwon-dev <taekwon_dev@naver.com>
2023-02-25 23:33:44 +0900

    의존성 관리 수정

--- spring/mvc/message_converter/src/test/java/com/youn/message_converter/v2/ControllerV2Test.java
+++ spring/mvc/message_converter/src/test/java/com/youn/message_converter/v2/ControllerV2Test.java
@@ -3,16 +3,11 @@ package com.youn.message_converter.v2;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.exc.InvalidDefinitionException;
-import com.fasterxml.jackson.databind.exc.MismatchedInputException;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
-import com.youn.message_converter.v0.RequestDtoV0;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
