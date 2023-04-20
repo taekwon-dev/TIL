e6ea25d9c6261ae872e62b9d5d4dcf79305e3dcf
taekwon-dev <taekwon_dev@naver.com>
2023-04-20 17:52:20 +0900

    Spring Advanced Spring AOP 구현

---
+++ spring-aop/aop/src/main/java/hello/aop/order/aop/AspectV6Advice.java
@@ -0,0 +1,31 @@
+package hello.aop.order.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+
+@Slf4j
+@Aspect
+public class AspectV6Advice {
+
+    @Before("hello.aop.order.aop.Pointcuts.orderAndService()")
+    public void doBefore(JoinPoint joinPoint) {
+        log.info("[before] {}", joinPoint.getSignature());
+    }
+
+    @AfterReturning(value = "hello.aop.order.aop.Pointcuts.orderAndService()", returning = "result")
+    public void doReturn(JoinPoint joinPoint, Object result) {
+        log.info("[return] {} return ={}", joinPoint.getSignature(), result);
+    }
+
+    @AfterThrowing(value = "hello.aop.order.aop.Pointcuts.orderAndService()", throwing = "ex")
+    public void doThrowing(JoinPoint joinPoint, Exception ex) {
+        log.info("[ex] {} message={}", joinPoint.getSignature(), ex.getMessage());
+    }
+
+    @After(value = "hello.aop.order.aop.Pointcuts.orderAndService()")
+    public void doAfter(JoinPoint joinPoint) {
+        log.info("[after] {}", joinPoint.getSignature());
+    }
+}
---
+++ spring-aop/aop/src/test/java/hello/aop/AopApplicationTests.java
@@ -0,0 +1,12 @@
+package hello.aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
