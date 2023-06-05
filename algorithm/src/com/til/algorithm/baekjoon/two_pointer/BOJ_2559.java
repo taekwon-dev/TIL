7aadee59d7144cf6300d49e43f3c43bf7b02d9de
taekwon-dev <taekwon_dev@naver.com>
2023-06-05 23:48:27 +0900

    06/05

--- algorithm/src/com/til/algorithm/baekjoon/two_pointer/BOJ_2559.java
+++ algorithm/src/com/til/algorithm/baekjoon/two_pointer/BOJ_2559.java
@@ -1,7 +1,6 @@
 package com.til.algorithm.baekjoon.two_pointer;
 
 import java.io.*;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BOJ_2559 {
@@ -9,36 +8,31 @@ public class BOJ_2559 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
         int[] arr = new int[N];
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(arr);
 
-        int X = Integer.parseInt(br.readLine());
-
-        int i = 0;
-        int j = N - 1;
         int sum = 0;
-        int answer = 0;
+        for (int i = 0; i < K; i++) {
+            sum += arr[i];
+        }
 
-        while (i < j) {
-            sum = arr[i] + arr[j];
-            if (sum == X) {
-                answer++;
-            }
-            if (sum < X) {
-                i++;
-            } else {
-                j--;
+        int max = sum;
+        for (int i = K; i < N; i++) {
+            sum += arr[i] - arr[i - K];
+            if (max < sum) {
+                max = sum;
             }
         }
 
-        bw.write(answer + "\n");
+        bw.write(max + "\n");
         bw.flush();
         bw.close();
         br.close();
